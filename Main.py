ce8a0e9260ad2553086118d4821ffc1515be74b0
Bewin Felix <99880789+Bewin007@users.noreply.github.com>
2022-09-10 20:42:04 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,13 @@
-length_of_circular_linked_list = int(input("Enter the length of the list:"))
-
-circular_linked_list = list(map(int,input("Enter the data:").strip().split(" ")))
-
+length_of_circular_linked_list = int(input())
+circular_linked_list = list(map(int,input().strip().split(" ")))
 actual_list = []
 
 value = 0
-
 while len(actual_list) < length_of_circular_linked_list and value < len(circular_linked_list):
     element = circular_linked_list[value]
     if element not in actual_list:
         actual_list.append(element)
     value += 1
 
-print("Length of the list is ",len(actual_list))
-print("The Actual list is "," ".join(str(num) for num in actual_list))
+print(len(actual_list))
+print(" ".join(str(num) for num in actual_list))
